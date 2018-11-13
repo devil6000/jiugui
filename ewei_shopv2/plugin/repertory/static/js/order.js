a9f8831913997679e0b2d88e3b17ea275f8c6127
devil5060 <devil5060@hotmail.com>
2018-11-13 16:27:20 +0800

    2018-11-13

--- ewei_shopv2/plugin/repertory/static/js/order.js
+++ ewei_shopv2/plugin/repertory/static/js/order.js
@@ -25,7 +25,7 @@ define(['core', 'tpl'], function(core, tpl) {
                     modal.changeTab(2)
                 },
                 shop: function () {
-
+                    modal.changeShop()
                 }
             }
         })
@@ -71,7 +71,25 @@ define(['core', 'tpl'], function(core, tpl) {
         })
     };
     modal.getShop = function() {
-
+        core.json('merch/list/ajaxmerchuser', {
+            page: modal.page
+        }, function (ret) {
+            var result = ret.result;
+            if(result.total <= 0){
+                $('.content-empty').show();
+                $('.fui-content').infinite('stop')
+            }else{
+                $('.content-empty').hide();
+                $('.fui-content').infinite('init');
+                if (result.list.length <= 0 || result.list.length < result.pagesize) {
+                    $('.fui-content').infinite('stop')
+                }
+            }
+            $('.content-loading').hide();
+            modal.page++;
+            core.tpl('#container', 'tpl_merch_list_user', result, modal.page > 1);
+            FoxUI.according.init();
+        })
     };
     return modal
 });
